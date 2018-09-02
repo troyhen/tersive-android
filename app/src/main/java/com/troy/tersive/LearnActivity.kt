2178caf8fbcccd2df59931c91b35e4a0e39f782b
troyhen <troyhen@gmail.com>
2018-09-01 20:07:26 -0600

    Update to androidx libraries

---
+++ app/src/main/java/com/troy/tersive/LearnActivity.kt
@@ -0,0 +1,6 @@
+package com.troy.tersive
+
+import androidx.appcompat.app.AppCompatActivity
+
+class LearnActivity : AppCompatActivity() {
+}
--- app/src/main/java/com/troy/tersive/MainActivity.kt
+++ app/src/main/java/com/troy/tersive/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.troy.tersive
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.design.widget.NavigationView
-import android.support.v4.view.GravityCompat
-import android.support.v7.app.ActionBarDrawerToggle
-import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 import kotlinx.android.synthetic.main.content_main.*
@@ -34,7 +34,8 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
 
-        nav_view.setNavigationItemSelectedListener(this)
+        val nav: NavigationView = findViewById(R.id.nav_view)
+        nav.setNavigationItemSelectedListener(this)
         hand_mode.isSelected = true
         setupListeners()
     }
