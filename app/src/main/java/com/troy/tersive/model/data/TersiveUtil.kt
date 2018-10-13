9241e7f95ab4e74d5d31f1903dcf76fba1cd6562
troyhen <troyhen@gmail.com>
2018-10-13 12:11:19 -0600

    Fix tersive conversion

--- app/src/main/java/com/troy/tersive/model/data/TersiveUtil.kt
+++ app/src/main/java/com/troy/tersive/model/data/TersiveUtil.kt
@@ -6,8 +6,14 @@ import javax.inject.Inject
 
 class TersiveUtil @Inject constructor() {
 
-    private val map = HashMap<String, String>()
-    private val buf = StringBuilder()
+    private val map = HashMap<CharSequence, String>()
+    private val output = StringBuilder()
+    private val word = StringBuilder()
+    private lateinit var text: CharSequence
+    private lateinit var words: Array<CharSequence>
+    private var index = 0
+    private var end = 0
+    private val ignored = hashSetOf("the")
 
     fun loadPhrases(phrases: List<Tersive>, key: Boolean) {
         map.clear()
@@ -19,95 +25,120 @@ class TersiveUtil @Inject constructor() {
     }
 
     fun toTersive(text: CharSequence): CharSequence {
-        val words = toWords(text)
-        buf.setLength(0)
-        var index = 0
-        val end = words.size
+//        val words = toWords(text)
+        this.text = text
+        output.setLength(0)
+        index = 0
+        end = text.length
+        words = arrayOf("", "", "", "", "")
+
+        var needed = words.size
         while (index < end) {
-            val word0 = words[index + 0]
-            val word1 = if (index + 1 < end) words[index + 1] else ""
-            val word2 = if (index + 2 < end) words[index + 2] else ""
-            val word3 = if (index + 3 < end) words[index + 3] else ""
-            val word4 = if (index + 4 < end) words[index + 4] else ""
+            fillWords(needed)
 
-            val phrase2 = "$word0 $word1"
-            val phrase3 = "$phrase2 $word2"
-            val phrase4 = "$phrase3 $word3"
-            val phrase5 = "$phrase4 $word4"
+            val phrase2 = "${words[0]} ${words[1]}"
+            val phrase3 = "$phrase2 ${words[2]}"
+            val phrase4 = "$phrase3 ${words[3]}"
+            val phrase5 = "$phrase4 ${words[4]}"
 
-            val tersive5 = map[phrase5]
-            val tersive4 = map[phrase4]
-            val tersive3 = map[phrase3]
+            val tersive1 = map[words[0]]
             val tersive2 = map[phrase2]
-            val tersive1 = map[word0]
+            val tersive3 = map[phrase3]
+            val tersive4 = map[phrase4]
+            val tersive5 = map[phrase5]
 
             when {
                 tersive5 != null -> {
-                    if (buf.isNotEmpty()) buf.append(' ')
-                    buf.append(tersive5)
-                    index += 5
+                    append(tersive5)
+                    needed = 5
                 }
                 tersive4 != null -> {
-                    if (buf.isNotEmpty()) buf.append(' ')
-                    buf.append(tersive4)
-                    index += 4
+                    append(tersive4)
+                    needed = 4
                 }
                 tersive3 != null -> {
-                    if (buf.isNotEmpty()) buf.append(' ')
-                    buf.append(tersive3)
-                    index += 3
+                    append(tersive3)
+                    needed = 3
                 }
                 tersive2 != null -> {
-                    if (buf.isNotEmpty()) buf.append(' ')
-                    buf.append(tersive2)
-                    index += 2
+                    append(tersive2)
+                    needed = 2
                 }
                 tersive1 != null -> {
-                    if (buf.isNotEmpty()) buf.append(' ')
-                    buf.append(tersive1)
-                    index++
+                    append(tersive1)
+                    needed = 1
                 }
-            }
-        }
-        return buf
-    }
-
-    private fun toWords(text: CharSequence): List<CharSequence> {
-        var start = 0
-        var state = LexState.BETWEEN
-        val words = mutableListOf<CharSequence>()
-        text.forEachIndexed { index, ch ->
-            val c = ch.toLowerCase()
-            if (state == LexState.PUNCTUATION) {
-                words.add(text.subSequence(start, index))
-                state = LexState.BETWEEN
-            }
-            when (state) {
-                LexState.WITHIN -> when {
-                    (c == '\u0027' || c == '\u2019') && index < text.length - 1 && text[index + 1].isLetterOrDigit() -> { // handle contraction
-                    }
-                    c.isWhitespace() || !c.isLetterOrDigit() -> {
-                        words.add(text.subSequence(start, index))
-                        state = LexState.BETWEEN
-                    }
+                ignored.contains(words[0]) -> {
                 }
                 else -> {
-                    when {
-                        c.isLetterOrDigit() -> {
-                            start = index
-                            state = LexState.WITHIN
-                        }
-                        !c.isWhitespace() -> {
-                            start = index
-                            state = LexState.PUNCTUATION
-                        }
-                        else -> words.add(text.subSequence(start, index + 1))
-                    }
+                    Timber.i("Unknown word '${words[0]}'")
+                    needed = 1
                 }
             }
         }
-        return words
+        return output
     }
+
+    private fun append(phrase: CharSequence) {
+        if (output.isNotEmpty()) output.append(' ')
+        output.append(phrase)
+    }
+
+    private fun fillWords(needed: Int) {
+        val last = words.size - 1
+        for (ix in 0.until(needed)) {
+            word.setLength(0)
+            loop@ while (index < text.length) {
+                val found = word.isNotEmpty()
+                val ch = text[index++].toLowerCase()
+                val isLetter = ch.isLetterOrDigit()
+                val isSpace = ch.isWhitespace()
+                when {
+                    found && !isLetter -> break@loop
+                    found || !isSpace -> word.append(ch)
+                }
+            }
+            System.arraycopy(words, 1, words, 0, last)
+            words[last] = word.toString()
+        }
+    }
+
+//    private fun toWords(text: CharSequence): List<CharSequence> {
+//        var start = 0
+//        var state = LexState.BETWEEN
+//        val words = mutableListOf<CharSequence>()
+//        text.forEachIndexed { index, ch ->
+//            val c = ch.toLowerCase()
+//            if (state == LexState.PUNCTUATION) {
+//                words.add(text.subSequence(start, index))
+//                state = LexState.BETWEEN
+//            }
+//            when (state) {
+//                LexState.WITHIN -> when {
+//                    (c == '\u0027' || c == '\u2019') && index < text.length - 1 && text[index + 1].isLetterOrDigit() -> { // handle contraction
+//                    }
+//                    c.isWhitespace() || !c.isLetterOrDigit() -> {
+//                        words.add(text.subSequence(start, index))
+//                        state = LexState.BETWEEN
+//                    }
+//                }
+//                else -> {
+//                    when {
+//                        c.isLetterOrDigit() -> {
+//                            start = index
+//                            state = LexState.WITHIN
+//                        }
+//                        !c.isWhitespace() -> {
+//                            start = index
+//                            state = LexState.PUNCTUATION
+//                        }
+//                        else -> words.add(text.subSequence(start, index + 1))
+//                    }
+//                }
+//            }
+//        }
+//        return words
+//    }
 
     fun optimizeHand(tersive: String): CharSequence {
         val buffer = StringBuilder(" ")
